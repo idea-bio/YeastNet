5c71f1f437cb62e3055ab7e09c1e7c30c7717db6
Danny <danny.salem2@gmail.com>
2021-02-28 20:44:49 -0500

    small changes to train pipeline

--- train.py
+++ train.py
@@ -13,19 +13,27 @@ from torch.utils.tensorboard import SummaryWriter
 ## Import Custom Modules
 from ynetmodel.detect import validate
 from Utils.helpers import getDatasetMoments
-from ynetmodel.defineNetwork import Net
+from ynetmodel.defineNetwork import NetOld as Net
 from ynetmodel.WeightedCrossEntropyLoss import WeightedCrossEntropyLoss
 from ynetmodel.YeastSegmentationDataset import YeastSegmentationDataset
+
 
 def main():
 
     ## Print Name/Info about Model Training Session
-    print('------------------------------------------------------')
-    print('|  Training with w0={}, sigma={}.\n|  Training Datasets: {}.\n|  Validation Datasets: {}'.format(loss_param[0], loss_param[1], trainingSets, testingSets),flush=True)
-    print('------------------------------------------------------')
+    print("------------------------------------------------------")
+    print(
+        "|  Training with w0={}, sigma={}.\n|  Training Datasets: {}.\n|  Validation Datasets: {}".format(
+            loss_param[0], loss_param[1], trainingSets, testingSets
+        ),
+        flush=True,
+    )
+    print("------------------------------------------------------")
     ## Start Timer, Tensorboard
     start_time = time.time()
-    writer = SummaryWriter(comment='_12test3,w0={},sigma={}'.format(loss_param[0], loss_param[1]))#log_dir="./logs")
+    writer = SummaryWriter(
+        comment="_12test3,w0={},sigma={}".format(loss_param[0], loss_param[1])
+    )  # log_dir="./logs")
 
     ## Instantiate Net/Optimizer
     net = Net()
@@ -35,46 +43,39 @@ def main():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     net.to(device)
 
-
-    if toResume=='True':
+    if toResume == "True":
         ## Load State
-        if allDatasets == 'False':
-            inputpath ="./NewNormModels/new_norm_testDSV%01dK%01d.pt" % (normtype,k) 
+        if allDatasets == "False":
+            inputpath = "./NewNormModels/new_norm_testDSV%01dK%01d.pt" % (normtype, k)
         else:
-            inputpath ="./NewNormModels/new_norm_testV%01dK%01d.pt" % (normtype,k) 
-       
+            inputpath = "./NewNormModels/new_norm_testV%01dK%01d.pt" % (normtype, k)
+
         checkpoint = torch.load(inputpath, map_location=device)
-        testIDs = checkpoint['testID']
-        trainIDs = checkpoint['trainID']
-        iteration = checkpoint['iteration']
-        start = checkpoint['epoch']
-        highestAccuracy = checkpoint['highestAccuracy']
-        net.load_state_dict(checkpoint['network'])
-        optimizer.load_state_dict(checkpoint['optimizer'])
+        testIDs = checkpoint["testID"]
+        trainIDs = checkpoint["trainID"]
+        iteration = checkpoint["iteration"]
+        start = checkpoint["epoch"]
+        highestAccuracy = checkpoint["highestAccuracy"]
+        net.load_state_dict(checkpoint["network"])
+        optimizer.load_state_dict(checkpoint["optimizer"])
 
     else:
         ## Intialize State
         iteration = 0
         start = 0
-        highestAccuracy = 0 
-
-        ## New two Dataset Training
-        #if trainTwo == 'True':
-        #    trainIDs = {
-        #        'DSDataset': list(range(150)),
-        #        'YITDataset1': list(range(60)),
-        #        #'YITDataset3': list(range(20)),
-        #    }
-        #    testIDs = {
-        #        #'DSDataset': list(range(150)),
-        #        #'YITDataset1': list(range(60)),
-        #        'YITDataset3': list(range(20)),
-        #    }
-        #trainIDs = {dataset: torch.load('./Utils/TrainingSplits/trainIDs.pt')[dataset] for dataset in trainingSets}
-        #testIDs = {dataset: torch.load('./Utils/TrainingSplits/testIDs.pt')[dataset] for dataset in testingSets}
-        trainIDs = {'DSDataset': list(range(150))}
-        testIDs = {'DSDataset': list(range(150))}
-
+        highestAccuracy = 0
+        if allDatasets:
+            trainIDs = {
+                dataset: torch.load("./Utils/trainIDs.pt")[k][dataset] for dataset in trainingSets
+            }
+            testIDs = {
+                dataset: torch.load("./Utils/testIDs.pt")[k][dataset] for dataset in testingSets
+            }
+        else:
+            trainIDs = {"DSDataset": torch.load("./Utils/trainIDs.pt")["DSDataset"]}
+            testIDs = {"DSDataset": torch.load("./Utils/testIDs.pt")["DSDataset"]}
+        # trainIDs = {"DSDataset": list(range(152))}
+        # testIDs = {"DSDataset": list(range(152))}
 
     ## Get Statistics of datasets
     trainSetMoments = getDatasetMoments(trainIDs)
@@ -84,34 +85,49 @@ def main():
     if torch.cuda.device_count() > 1:
         net = torch.nn.DataParallel(net)
 
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', factor=0.8, patience=50, verbose=True)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer, "min", factor=0.8, patience=50, verbose=True
+    )
 
     ## Instantiate Training and Validation DataLoaders
-    trainDataSet = YeastSegmentationDataset(trainIDs, crop_size = crop_size, random_rotate = random_rotate, random_flip = random_flip,
-                                            no_og_data = no_og_data, random_crop=random_crop,
-                                            setMoments = trainSetMoments, loss_param=loss_param)
-    trainLoader = torch.utils.data.DataLoader(trainDataSet, batch_size=1,
-                                            shuffle=True, num_workers=0)
-                                            
-    testDataSet = YeastSegmentationDataset(testIDs, setMoments = testSetMoments)
-    testLoader = torch.utils.data.DataLoader(testDataSet, batch_size=1,
-                                            shuffle=False, num_workers=0)
+    trainDataSet = YeastSegmentationDataset(
+        trainIDs,
+        crop_size=crop_size,
+        random_rotate=random_rotate,
+        random_flip=random_flip,
+        no_og_data=no_og_data,
+        random_crop=random_crop,
+        setMoments=trainSetMoments,
+        loss_param=loss_param,
+    )
+    trainLoader = torch.utils.data.DataLoader(
+        trainDataSet, batch_size=1, shuffle=True, num_workers=0
+    )
+
+    testDataSet = YeastSegmentationDataset(testIDs, setMoments=testSetMoments)
+    testLoader = torch.utils.data.DataLoader(
+        testDataSet, batch_size=1, shuffle=False, num_workers=0
+    )
 
     ## Set Training hyperparameters/conditions
     criterion = WeightedCrossEntropyLoss()
-    classes = ('background','cell')
+    classes = ("background", "cell")
 
     ## Epoch Loop: first loops over batches, then over validation set
-    for epoch in range(start,end):  
-        
+    for epoch in range(start, end):
+
         net.train()
         ## Batch Loop
         for i, data in enumerate(trainLoader, 0):
             ## Total iteration
-            iteration+=1
+            iteration += 1
             ## Get inputs
             trainingImage, mask, lossWeightMap = data
-            trainingImage, mask, lossWeightMap = trainingImage.to(device), mask.to(device), lossWeightMap.to(device)
+            trainingImage, mask, lossWeightMap = (
+                trainingImage.to(device),
+                mask.to(device),
+                lossWeightMap.to(device),
+            )
 
             ## Zero the parameter gradients
             optimizer.zero_grad()
@@ -121,26 +137,25 @@ def main():
 
             ## Calculate and Write Loss
             if torch.isnan(outputs).any() > 0:
-                print('There Be Nans')
+                print("There Be Nans")
             loss = criterion(outputs, mask.long(), lossWeightMap)
-            
+
             ## Backpropagate Loss
             loss.backward()
-
             ## Update Parameters
-            
+
             optimizer.step()
 
         ## Epoch validation
 
-        val_acc = validate(net, device, testLoader, criterion, saveImages = True)
+        val_acc = validate(net, device, testLoader, criterion, saveImages=True)
         scheduler.step(val_acc)
-        
-        print('[%d, %d] IntOfUnion (Cell): %.5f \n' % (iteration, epoch + 1, val_acc),flush=True)
-        writer.add_scalar('Validation Cell IOU', val_acc, global_step=epoch, walltime=time.time())
+
+        print("[%d, %d] IntOfUnion (Cell): %.5f \n" % (iteration, epoch + 1, val_acc), flush=True)
+        writer.add_scalar("Validation Cell IOU", val_acc, global_step=epoch, walltime=time.time())
         ## Epoch Time
         elapsed_time = time.time() - start_time
-        print(str(elapsed_time / 60) + 'min',flush=True)
+        print(str(elapsed_time / 60) + "min", flush=True)
 
         if val_acc > highestAccuracy:
             save_option = True
@@ -148,8 +163,8 @@ def main():
         else:
             save_option = False
 
-        ## Save Model 
-        if save_option: #saveCP:
+        ## Save Model
+        if save_option:  # saveCP:
             try:
                 net_state_dict = net.module.state_dict()
             except AttributeError:
@@ -164,54 +179,62 @@ def main():
                 "iteration": iteration,
                 "highestAccuracy": val_acc,
             }
-            
-            #outputpath = modelfolder + '{}_w0={}_sigma={}.pt'.format(trainingSets, loss_param[0], loss_param[1])
+
+            # outputpath = modelfolder + '{}_w0={}_sigma={}.pt'.format(trainingSets, loss_param[0], loss_param[1])
             outputpath = modelfolder + modelname
             torch.save(checkpoint, outputpath)
 
     ## Finish
     elapsed_time = time.time() - start_time
-    print('Finished Training, Duration: seconds' + str(elapsed_time),flush=True)
-    with open('ScreenOutput.txt','a') as outputfile:
-        outputfile.writelines('w0={},sigma={},accuracy={:.4f}\n'.format(loss_param[0], loss_param[1], 
-                                                                checkpoint['highestAccuracy']))
+    print("Finished Training, Duration: seconds" + str(elapsed_time), flush=True)
+    with open("ScreenOutput.txt", "a") as outputfile:
+        outputfile.writelines(
+            "w0={},sigma={},accuracy={:.4f}\n".format(
+                loss_param[0], loss_param[1], checkpoint["highestAccuracy"]
+            )
+        )
     writer.close()
 
-if __name__ == '__main__':
 
-## Load Settings YAML file
-    if os.path.exists("settings.yml"):        
-        settings=yaml.load(open("settings.yml","r"))
+if __name__ == "__main__":
+
+    ## Load Settings YAML file
+    if os.path.exists("settings.yml"):
+        settings = yaml.load(open("settings.yml", "r"))
     train_param = {}
-## Try loading settings from environment variable
+    ## Try loading settings from environment variable
     try:
-        k = int(os.environ['K_FOLD'])
-        toResume = os.environ['RESUME']
-        normtype = os.environ['NORMTYPE']
-        #allDatasets = os.environ['ALLDATASETS']
-        loss_param = [os.environ['W0'],os.environ['SIGMA']]
-        dataset = os.environ['DATASET']
+        k = int(os.environ["K_FOLD"])
+        toResume = os.environ["RESUME"]
+        allDatasets = os.environ["ALLDATASETS"]
+        loss_param = [os.environ["W0"], os.environ["SIGMA"]]
+        modelname = os.environ["MODEL_NAME"]
+
     except KeyError:
-        k = settings['train_param']['k']
-        toResume = settings['train_param']['toResume']
-        normtype = settings['train_param']['normtype']
-        #allDatasets = settings['data']['allDatasets']
-        loss_param = settings['data']['loss_param']
-        trainingSets = settings['data']['trainingSets']
-        testingSets = settings['data']['testingSets']
-        
+        k = settings["train_param"]["k"]
+        toResume = settings["train_param"]["toResume"]
+        allDatasets = settings["train_param"]["allDatasets"]
+        loss_param = settings["data"]["loss_param"]
+        modelname = settings["model"]["modelname"]
 
-    #loss_param = ['10','5'] if dataset == 'DSDataset' else ['5','5']
+    trainingSets = settings["data"]["trainingSets"]
+    testingSets = settings["data"]["testingSets"]
+    # loss_param = ['10','5'] if dataset == 'DSDataset' else ['5','5']
+    normtype = settings["train_param"]["normtype"]
+    end = settings["train_param"]["end"]
+    lr = settings["train_param"]["learning_rate"]
+    momentum = settings["train_param"]["momentum"]
+    modelfolder = settings["model"]["folderpath"]
+    no_og_data = settings["train_param"]["no_og_data"]
+    random_flip = settings["train_param"]["random_flip"]
+    random_rotate = settings["train_param"]["random_rotate"]
+    random_crop = settings["train_param"]["random_crop"]
+    crop_size = settings["train_param"]["crop_size"]
+    batch_size = settings["train_param"]["batch_size"]
 
-    end = settings['train_param']['end']
-    lr = settings['train_param']['learning_rate']
-    momentum = settings['train_param']['momentum']
-    modelfolder = settings['model']['folderpath']
-    no_og_data = settings['train_param']['no_og_data']
-    random_flip = settings['train_param']['random_flip']
-    random_rotate = settings['train_param']['random_rotate']
-    random_crop = settings['train_param']['random_crop']
-    crop_size = settings['train_param']['crop_size']
-    batch_size = settings['train_param']['batch_size']
+    if allDatasets == "False":
+        modelname = f"new_norm_testDSV3K{k}.pt"
+    else:
+        modelname = f"new_norm_testV3K{k}.pt"
 
     main()
--- ynetmodel/detect.py
+++ ynetmodel/detect.py
@@ -3,17 +3,18 @@ import torch
 import imageio
 import numpy as np
 import pdb
+
 ## Import Custom Code
 from Utils.helpers import accuracy
 from ynetmodel.defineNetwork import NetOld
 
 
-def infer(images, num_images, device = "cpu", model_path = "./model_cp.pt"):
+def infer(images, num_images, device="cpu", model_path="./model_cp.pt"):
     ## Instantiate Net, load parameters
     net = NetOld()
     net.eval()
     checkpoint = torch.load(model_path, map_location=device)
-    net.load_state_dict(checkpoint['network'])
+    net.load_state_dict(checkpoint["network"])
 
     ## Move Net to GPU
     net.to(device)
@@ -31,7 +32,8 @@ def infer(images, num_images, device = "cpu", model_path = "./model_cp.pt"):
 
     return output
 
-def validate(net, device, testLoader, criterion = None, saveImages = False):
+
+def validate(net, device, testLoader, criterion=None, saveImages=False):
 
     ## Run net without regularization techniques
     net.eval()
@@ -44,23 +46,30 @@ def validate(net, device, testLoader, criterion = None, saveImages = False):
         ## Get inputs and transfer to GPU
         image, mask, _ = data
         image = image.to(device)
-       
         ## Run Batch through data with no grad
         with torch.no_grad():
             outputs = net(image.float())
-        predictions = outputs.cpu().detach().numpy()[0,:,:,:]
+        predictions = outputs.cpu().detach().numpy()[0, :, :, :]
         maskPrediction = (predictions[1] > predictions[0]) * 1
 
         ## Calculate Accuracy and update running total
-        _, IntOfUnion = accuracy(mask.cpu().detach().numpy()[0,:,:,0], maskPrediction)
+        _, IntOfUnion = accuracy(mask.cpu().detach().numpy()[0, :, :, 0], maskPrediction)
         runningIOU += IntOfUnion[1]
-        
+
         ## Output Images
         if saveImages:
-            #pdb.set_trace()
-            imageio.imwrite('Validation/' + str(i) + 'Pred.png', (maskPrediction.astype('uint8'))* 255)
-            imageio.imwrite('Validation/' + str(i) + 'IMG.png', (image[0,0,:,:].cpu().detach().numpy() * 255).astype('uint8'))
-            imageio.imwrite('Validation/' + str(i) + 'True.png', (mask[0,:,:,0].cpu().detach().numpy() * 255).astype('uint8'))
+            # pdb.set_trace()
+            imageio.imwrite(
+                "Validation/" + str(i) + "Pred.png", (maskPrediction.astype("uint8")) * 255
+            )
+            imageio.imwrite(
+                "Validation/" + str(i) + "IMG.png",
+                (image[0, 0, :, :].cpu().detach().numpy() * 255).astype("uint8"),
+            )
+            imageio.imwrite(
+                "Validation/" + str(i) + "True.png",
+                (mask[0, :, :, 0].cpu().detach().numpy() * 255).astype("uint8"),
+            )
 
     ## Return the mean Loss
     return runningIOU / i
