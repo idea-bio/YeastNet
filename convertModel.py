3e1ec801d4378e14f8b41b75bf831189435afd41
Chizkiyahu Raful <chizkiyahu-r@idea-bio.com>
2021-12-20 15:04:49 +0200

    update convertModel.py to support dynamic input/output

--- convertModel.py
+++ convertModel.py
@@ -1,16 +1,36 @@
 import torch
-
+import os
 from ynetmodel.detect import LoadModel
 
-org = LoadModel('../Model/YNMOdelParams.pt')
 
-#script = torch.jit.script(org)
+modelPath = 'C:\code\YeastNet\Model\YeastNet2Param.pt'
+assert os.path.exists(modelPath)
+org = LoadModel(modelPath)
+script = torch.jit.script(org)
+
 #script.save('model/script.pt')
-
 #trace = torch.jit.trace(org, torch.rand(1,1,1024,1024),)
 #trace.save('model/trace.pt')
 
 dummy_input = torch.randn(1, 1, 1024, 1024)
-torch.onnx.export(org,dummy_input,"../Model/yeast.onnx")
+folder = "C:\code\YeastNet\Model"
+if not os.path.exists(folder):
+    os.mkdir(folder)
+f = os.path.join(folder, "yeast.onnx")
+torch.onnx.export(model=script,
+                  args=dummy_input,
+                  example_outputs=dummy_input,
+                  f=f,
+                  input_names= ['input'] , output_names=['output'],
+                  dynamic_axes={'input':{2:'width',
+                                         3:'height'},
 
+                                  'output':{1:'lables',
+                                            2:'width',
+                                            3:'height'
+                                            }}
+                  )
 
+f = os.path.join(folder, "yeast_org.onnx")
+torch.onnx.export(org,dummy_input,f=f)
+print(f"save file to {f}")
